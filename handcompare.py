8878c876dc3af788a9e3b559cbf0694e8179cd51
bkownacki1 <54732696+bkownacki1@users.noreply.github.com>
2019-10-28 19:32:25 -0500

    Removed irrelevant data from HandInfo, added part of CNN Alg to handcompare
    Removed the columns for age, gender, skinColor, accessories, nailPolish, aspectOfHand, and irregularities from HandInfo and put the new stripped txt into a new \t separated txt file called "HandInfoStripped.txt". I also added back the aspectOfHand and made a new text file called "HandInfoWithPos.txt" just in case that comes in handy for the future.
    
    For handcompare.py I added some of the code from part one of:
    
    https://towardsdatascience.com/image-classification-python-keras-tutorial-kaggle-challenge-45a6332a58b8
    
    but this code is for a CNN and I might end up using multiple DCNNs like is done in facial recognition. The idea being that hand recognition is bascially the same thing as facial recognition, just with a different unique body part. If you can recognize a face you can recognize a hand.
    
    I also may end up scaling down the input images for faster processing, but that may cause data loss that is more significant for hand recognition than for face recogntion but I will see when I start training.

--- handcompare.py
+++ handcompare.py
@@ -1,1 +1,30 @@
+from __future__ import print_function
+
 import keras
+import os
+import imageio
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+naming_dict={}
+f = open("HandInfoStripped.txt", "r")
+fileContents = f.read()
+fileContents = fileContents.split('\t')
+for i in range(len(fileContents)-1):
+    fileContents[i] = fileContents[i].split(',')
+    naming_dict[fileContents[i][0]] = fileContents[i][1]
+
+
+
+batch_size = 1000
+num_classes = 2
+epochs = 12
+
+img_rows, img_cols = 1200, 1600
+
+(x_train, y_train), (x_test,y_test) = 0
+
+print("")
